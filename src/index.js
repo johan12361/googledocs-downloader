25f45f59d13a62ea5d160a90fa8f85b7cb3669a4
johan12361 <johan12361@gmail.com>
2024-09-02 22:01:13 -0500

    +funcion savetxt()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { getTxtDoc } from './getTxt/getTxt.mjs'
 import { getIdDocFromUrl } from './format/getId.mjs'
+import { savetxt } from './save/saveTxt.mjs'
 
-export { getTxtDoc, getIdDocFromUrl }
+export { getTxtDoc, getIdDocFromUrl, savetxt }
